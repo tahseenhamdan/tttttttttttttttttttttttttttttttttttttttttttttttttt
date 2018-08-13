669634af60caa22ccefc77680f4eb978341786c9
tahseenhamdan <41524266+tahseenhamdan@users.noreply.github.com>
2018-08-13 12:05:06 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+
+**سلام عليكم يا قلبي 
+جبت لكم افضل سيرفر سوالف 
+تنورنا يحلو
+love you
+
+                               [ https://discord.gg/EwrnsxW
+] 
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم يا قلبي 
+جبت لكم افضل سيرفر سوالف 
+تنورنا يحلو
+love you
+
+                               [ https://discord.gg/EwrnsxW
+ ] 
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+client.login('NDY5NzIxMDU3MTk3MjkzNTY4.DlBBfA.Z_ZRmsZ8C5ZVdssna72kDqf3TOo');
